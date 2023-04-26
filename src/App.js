3141bed82120b9334f72b88c3b75115b1965a51e
aayushmandavya <mandavyaaayush98@gmail.com>
2023-04-26 13:07:03 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from "react";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const ChangeSign = () => {
+
+    if (result.slice(0, 1) === "-") {
+      setResult("+".concat(result));
+    } else {
+      setResult("-".concat(result));
+    }
+
+  };
+
+  function handleClick(e) {
+    setResult(result.concat(e.target.name));
+  }
+
+  function clear() {
+    setResult("");
+  }
+
+  function calculate() {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Calculator__wrap">
+      <from>
+        <input type="text" className="Display" value={result}/>
+      </from>
+
+      <div className="Calculator__button">
+        <button id="clear" onClick={clear}>
+          AC
+        </button>
+        <button id="changeSign" onClick={ChangeSign}>
+          +/-
+        </button>
+        <button name="%" onClick={handleClick}>
+          %
+        </button>
+        <button className="Divide" name="/" onClick={handleClick}>
+          /
+        </button>
+        <button name="7" onClick={handleClick}>
+          7
+        </button>
+        <button name="8" onClick={handleClick}>
+          8
+        </button>
+        <button name="9" onClick={handleClick}>
+          9
+        </button>
+        <button name="*" className="Multiply" onClick={handleClick}>
+          &times;
+        </button>
+        <button name="4" onClick={handleClick}>
+          4
+        </button>
+        <button name="5" onClick={handleClick}>
+          5
+        </button>
+        <button name="6" onClick={handleClick}>
+          6
+        </button>
+        <button name="-" className="Minus" onClick={handleClick}>
+          -
+        </button>
+        <button name="1" onClick={handleClick}>
+          1
+        </button>
+        <button name="2" onClick={handleClick}>
+          2
+        </button>
+        <button name="3" onClick={handleClick}>
+          3
+        </button>
+        <button name="+" className="Plus" onClick={handleClick}>
+          +
+        </button>
+        <button name="0" id="zero" onClick={handleClick}>
+          0
+        </button>
+        <button name="." onClick={handleClick}>
+          .
+        </button>
+        <button id="result" className="Result" onClick={calculate}>
+          =
+        </button>
+      </div>
     </div>
   );
 }
